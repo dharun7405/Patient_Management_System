54111363c10a7f883941049513d73095f9024903
dharun7405 <dharunsudha74@gmail.com>
2025-06-15 11:24:26 +0530

    deletePatient added

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -7,8 +7,7 @@ import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("/patients")
@@ -34,5 +33,11 @@ public class PatientController {
     public ResponseEntity<PatientResponseDTO>updatePatient(@PathVariable UUID id,
                                                            @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
         return ResponseEntity.ok().body(patientService.updatePatient(id,patientRequestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id){
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
     }
 }
