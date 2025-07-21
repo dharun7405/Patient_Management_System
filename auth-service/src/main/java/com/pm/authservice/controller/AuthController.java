6b6cdc39a65f3f7924d3aa69e1efdc47471ef8dc
dharun7405 <dharunsudha74@gmail.com>
2025-07-21 22:02:14 +0530

    Login endpoint added in controller

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.pm.authservice.controller;
 
 import com.pm.authservice.dto.*;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 @RestController
 public class AuthController {
@@ -11,6 +13,11 @@ public class AuthController {
     @Operation(summary = "Generate token on User login")
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO){
-
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+        if(tokenOptional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String token = tokenOptional.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 }
