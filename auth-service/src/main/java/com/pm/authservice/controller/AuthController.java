705cdf3821321cd086638512212260f9591e439b
dharun7405 <dharunsudha74@gmail.com>
2025-07-27 16:03:25 +0530

    Auth service created

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -1,14 +1,16 @@
 package com.pm.authservice.controller;
 
 import com.pm.authservice.dto.*;
+import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {
+
+    private AuthService authService;
 
     @Operation(summary = "Generate token on User login")
     @PostMapping("/login")
