d4c7022a492be3c007f0e31d82e0b544557bbcee
dharun7405 <dharunsudha74@gmail.com>
2025-07-27 16:46:46 +0530

    password encoding added

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.pm.authservice.service;
 
 import com.pm.authservice.dto.LoginRequestDTO;
-import com.pm.authservice.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
@@ -9,12 +9,16 @@ import java.util.Optional;
 public class AuthService {
 
     private final UserService userService;
-    public AuthService(UserService userService){
+    private final PasswordEncoder passwordEncoder;
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder){
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO){
-        Optional<User> user = userService.findByEmail(loginRequestDTO.getEmail())
-                .filter()
+        Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(),u.getRole()));
+        return token;
     }
 }
